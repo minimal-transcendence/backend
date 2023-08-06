8fd16232c71be749a240416be621feeacd2abc66
namkim <namkim@student.42seoul.kr>
2023-08-06 20:12:16 +0900

    namkim : save : storeServices

--- src/backend/src/chat/chat.gateway.ts
+++ src/backend/src/chat/chat.gateway.ts
@@ -12,6 +12,7 @@ import {
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { Server } from 'socket.io';
+// import { AuthService } from 'src/auth/auth.service';
 
 // export type objectRoom
 
@@ -32,13 +33,10 @@ export class ChatGateway
 	server : Server;
 
 	private logger = new Logger('Gateway');
+	// constructor (private readonly authService : AuthService){}	//guard는 쓸 수 없다는 이야기가 있다
 
 	//connection_error 어디서 처리...?
 	// afterInit(){
-	// 	this.server.on('create-room', (room) => {
-	// 		//
-	// 		this.logger.log(`"Room: ${ room }" 이 생성되었습니다.`);
-	// 	})
 	// }
 	
 	//socket 연결 시
@@ -51,6 +49,8 @@ export class ChatGateway
 		return this.server.sockets.adapter.rooms.get(roomName)?.size;
 	}
 
+	//sids: Map<SocketId, Set<Room>>
+	//rooms: Map<Room, Set<SocketId>>
 	getRooms() : object {
 		const {
 			sockets: {
@@ -58,46 +58,145 @@ export class ChatGateway
 			},
 		} = this.server;
 		const roomList = [];
-		console.log(rooms.size);
-		console.log(sids.size);
-		rooms.forEach((element, key) => {
-			// console.log(`_[0] : ${_.values()[0]}, _[1] : ${_[1]}, ${key}, ${sids.get(key)}, `);
-			console.log(`${element}, ${key}`);
-			const thisArray = sids.get(key);
-			console.log(`my array: ${thisArray}`);
-			thisArray.forEach((element) => {
-				console.log(element);
-			});
-			console.log(`my array end`);
-			if (sids.get(key) === undefined){
+		rooms.forEach((_, key) => {
+			if (_.has(key))
+				console.log("it is " + key);
+			else
+				console.log("it is not" + key);
+
+			if (sids.get(key) === undefined){	//왜 undefined?????
 				roomList.push(key);
 			}
-			console.log(roomList);
 		});
+		//첫째 인자에 user랑 room이 둘 다
+		//두번째 인자에는 userId만 있는 듯? 아니 대체 왜????
+		//왜 실제로는 둘 다 첫번째 인자가 forEach 를 돌까?
+		// sids.forEach((a, b) => {
+		// 	console.log(`a:`);
+		// 	a.forEach((sth) => {
+		// 		console.log(sth);
+		// 	})
+		// 	console.log(`b: ${b}`);
+		// })
+		// //세번째 인자
+		// //4번째 인자
+		// rooms.forEach((c, d) => {
+		// 	console.log(`c:`);
+		// 	c.forEach((sth) => {
+		// 		console.log(sth);
+		// 	})
+		// 	console.log(`d: ${d}`);
+		// })
+		rooms.forEach((c, d, e) => {
+			console.log(c);
+			console.log(d);
+			console.log(e);
+			// Set(1) { 'wGDDKwfSFrXGEqFzAAAB' }
+			// wGDDKwfSFrXGEqFzAAAB
+			// Map(2) {
+			//   'wGDDKwfSFrXGEqFzAAAB' => Set(1) { 'wGDDKwfSFrXGEqFzAAAB' },
+			//   'room3' => Set(1) { 'wGDDKwfSFrXGEqFzAAAB' }
+			// }
+			// Set(1) { 'wGDDKwfSFrXGEqFzAAAB' }
+			// room3
+			// Map(2) {
+			//   'wGDDKwfSFrXGEqFzAAAB' => Set(1) { 'wGDDKwfSFrXGEqFzAAAB' },
+			//   'room3' => Set(1) { 'wGDDKwfSFrXGEqFzAAAB' }
+			// }
+		})
+		/*
+		생각한 내용이 아니었다...!
+		a:
+		tNW5AmGE_lG8GZe1AAAB
+		room1
+		b: tNW5AmGE_lG8GZe1AAAB
+		c:
+		tNW5AmGE_lG8GZe1AAAB
+		d: tNW5AmGE_lG8GZe1AAAB
+		c:
+		tNW5AmGE_lG8GZe1AAAB
+		d: room1
+		*/
+		/*
+		a:
+		n0N0atO_oG_SL5TXAAAB
+		room1
+		room2
+		b: n0N0atO_oG_SL5TXAAAB
+		a:
+		bBRv2kwFz4xVmldXAAAD
+		room3
+		b: bBRv2kwFz4xVmldXAAAD
+		c:
+		n0N0atO_oG_SL5TXAAAB
+		d: n0N0atO_oG_SL5TXAAAB
+		c:
+		n0N0atO_oG_SL5TXAAAB
+		d: room1
+		c:
+		n0N0atO_oG_SL5TXAAAB
+		d: room2
+		c:
+		bBRv2kwFz4xVmldXAAAD
+		d: bBRv2kwFz4xVmldXAAAD
+		c:
+		bBRv2kwFz4xVmldXAAAD
+		d: room3
+		*/
 		return roomList;
 	}
 
-	handleConnection(@ConnectedSocket() socket: Socket) {
-		console.log(this.server.engine.clientsCount);
-		socket.data.nickname = "Anonymous";	//or how can you get given nickname?
-		console.log("default nick : " + socket.data.nickname);
-		socket.on("enter_room", (roomName, nickname, done) => {
-			socket.join(roomName);
-			// done();	//done?
-			this.setNickname(socket, nickname);
-			socket.to(roomName).emit("welcome", socket.data.nickname, this.countRoom(roomName));	//감지되지 않음
-			console.log("after nick : " + socket.data.nickname);
-			this.server.sockets.emit("update_rooms", this.getRooms());
+	async handleConnection(@ConnectedSocket() client: Socket) {
+		/* 1. verification */
+		//https://stackoverflow.com/questions/58670553/nestjs-gateway-websocket-how-to-send-jwt-access-token-through-socket-emit
+		/*
+		const payload = this.authService.verification{
+			client.handshake.headers.authorization,
+		};
+		const user = await this.userService.findOnd(payload.userId);
+		!user && client.disconnect();
+		*/
+		/* 
+		https://socket.io/docs/v3/server-api/
+		io.on("connection", (socket) => {
+		socket.use(([event, ...args], next) => {
+			if (isUnauthorized(event)) {
+			return next(new Error("unauthorized event"));
+			}
+			// do not forget to call next
+			next();
+		});
+		socket.on("error", (err) => {
+			if (err && err.message === "unauthorized event") {
+			socket.disconnect();
+			}
+		});
+		});
+		*/
+
+		/* 2. reuse socket id & identify unique user */
+		//pass 하면 manually give session id : session id 안 부여할 수는 없나?
+		//https://stackoverflow.com/questions/18294620/reuse-socket-id-on-reconnect-socket-io-node-js
+		
+		/* 3. handle! */
+		client.on("enter_room", (roomName, nickName) => {
+			console.log("[1] : " + this.server.sockets.adapter.rooms.size);
+			//string.startWith (아래 형식 가능) --> user와 이름이 같은 
+			client.join(`#${roomName}$${roomName}`);
+			console.log("[2] : " + this.server.sockets.adapter.rooms.size);
+			this.getRooms();
 		})
+
 	}
 	
 	//socket 중단 시
 	//user일 때 -> 들어간 모든 채팅방에서 나가기
 	//room 은 자동으로 관리?
 	// 클라이언트 비정상 종료? : 브라우저 강제종료 & 하드웨어 강제종료 시 감지 못 함
-	// handleDisconnect(@ConnectedSocket() socket: Socket) {
-
-	// }
+	// 누군가 접속이 끊어지면 동작
+	handleDisconnect(@ConnectedSocket() socket: Socket) {
+		//다른 유저의 화면 업데이트 : 채팅화면의 유저 리스트 갱신
+	}
 
 	@SubscribeMessage('ClientToServer')
 	async handleMessage(
@@ -105,7 +204,7 @@ export class ChatGateway
 		@ConnectedSocket() client : Socket
 	) {
 		console.log(data);
-		client.emit('ServerToClient', data);
+		client.emit('ServerToClient', data);	//array로 들어오는 듯?
 		this.server.emit('ServerToClient', "we have new client!");
 	}
 }
--- src/backend/src/chat/chat.user-store.service.ts
+++ src/backend/src/chat/chat.user-store.service.ts
@@ -1,15 +1,48 @@
 import { Injectable } from '@nestjs/common';
 
 class User {
+	//속성 private?
 	// id : number;	//어차피 map에 있을건데 여기 있을 필요있...?
 	nickname : string;	//as nickname? or will you give me a chance to change nickname?
 		//만약 nick으로 하면 바꿀 때를 구현하기 힘들 것 같다...
 		//아니면 이전까지 기록을 싹 갈아치운 다음에 이걸 가는 것도 가능할 것 같기도?
-	blocklist : string[];
+	blocklist : Set<number>;
 	connected : boolean;
 	//들어간 방 모두 가지고 있어야 하나?
-	joinedRooms : string[];
+	joinlist : Set<string>;
 	// currentRoom : string; <- message 전부 store하는 시점에서 필요없는 것 같음
+
+	constructor(nickname : string){
+		this.nickname = nickname;
+		this.blocklist = new Set();
+		this.connected = true;
+		this.joinlist = new Set();
+	};
+
+	addUserToBlocklist(userid : number){
+		this.blocklist.add(userid);
+	};
+
+	deleteUserFromBlockList(userid : number){
+		this.blocklist.delete(userid);	//없는 경우 뭔가 조치 필요?
+	}
+
+	addRoomToJoinlist(roomname : string){
+		this.joinlist.add(roomname);
+	}
+
+	deleteRoomFromJoinList(roomname : string){
+		this.joinlist.delete(roomname);
+	}
+
+	//getter는 필요없음
+	clearBlocklist(){
+		this.blocklist.clear();
+	}
+
+	clearJoinlist(){
+		this.joinlist.clear();
+	}
 }
 
 interface UserStore {
