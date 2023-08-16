63b615bebe38f8620ef3966cf96973d317d5259e
namkim <namkim@student.42seoul.kr>
2023-08-16 22:41:50 +0900

    namkim : add GameMatchStoreServices

--- src/backend/src/store/store.match.service.ts
+++ src/backend/src/store/store.match.service.ts
@@ -16,11 +16,15 @@ export class Match {
 
 //id를 부여할지...?
 export class Challenge {
+	// id : number;
 	from : number;
 	to : number;
-	constructor (from : number, to : number){
+	mode : number;
+	constructor (id : number, from : number, to : number, mode : number){
+		// this.id = id;
 		this.from = from;
 		this.to = to;
+		this.mode = mode;
 	}
 }
 
@@ -35,18 +39,20 @@ interface MatchStore {
 }
 
 interface ChallengeStore {
-	challenges : Map<Challenge, number>	//생각해보니 map이 더 맞는거 아닌가? 같은 유저한테 여러번 할 수 없다면???
-						//삽입은 차치하고 중간에 빠져야 하는 경우도 생각하면.... map이 더 맞는 것 같다.
+	challenges : Challenge[];	//생각해보니 map이 더 맞는거 아닌가? 같은 유저한테 여러번 할 수 없다면???
 	getAllChallengeBySender(from : number) : Challenge[];
 	getAllChallengeByReceiver(to : number) : Challenge[];
 	saveNewChallenge(id : number, from : number, to: number, mode : number) : void;
-	getChallengeById(id : number) : Challenge;
+	findChallengeByPair(from : number, to : number) : Challenge;
+	popChallengeByPair(from : number, to : number) : Challenge;
 }
 
 interface RandomQueue {
-	queue : Map<number, number[]>	//이게 맞아...? 아니면 Map<number, User> 정도는 해줘야하나...? or Map<number, number[]>도 가능
+	queue : Map<number, number>
+	// queue : Map<number, number[]>	//이게 맞아...? 아니면 Map<number, User> 정도는 해줘야하나...? or Map<number, number[]>도 가능
 	getFirstComerByMode(mode : number) : number;
-	waitByMode(mode : number, userid : number) : void;
+	saveInQueueByMode(mode : number, userid : number) : void;
+	removeFromQueueByMode(mode : number) : void;
 }
 
 @Injectable()
@@ -95,9 +101,45 @@ export class GameMatchStoreService implements MatchStore, ChallengeStore, Random
 		this.challenges.push(new Challenge(id, from, to, mode));
 	}
 
-	//으음
-	getChallengeById(id: number): Challenge {
-		const res = this.challenges.find(challenge => challenge === id);
-		return (res);
+	//으음.... to / from이 반대 방향인 것도 찾아줄건가...? 그게 좋을 것 같다...
+	//CHECK : null로 return 하기 vs undefined로 return하기
+	//CHECK : array로 return되는지?
+	//꼭... splice를 해야하는지..? 필요한 상황이 있고 안 필요한 상황이 있는데...
+	findChallengeByPair(from : number, to : number): Challenge {
+		// const res = this.challenges.find(challenge => challenge === id);
+		const index = this.challenges.findIndex(
+			(data) => (data.from === from && data.to === to) ||
+					(data.from === to && data.to === from));
+		if (index === -1)
+			return (undefined);
+		else {
+			// return (this.challenges.splice(index, 1)[0]);
+			return (this.challenges)[index];
+		}
+	}
+	
+	popChallengeByPair(from : number, to : number) : Challenge {
+		const index = this.challenges.findIndex(
+			(data) => (data.from === from && data.to === to) ||
+					(data.from === to && data.to === from));
+		if (index === -1)
+			return (undefined);
+		else {
+			return (this.challenges.splice(index, 1)[0]);
+		}
+	}
+
+	//없으면? undefined
+	getFirstComerByMode(mode: number): number {
+		return(this.queue.get(mode));
+	}
+
+	saveInQueueByMode(mode: number, userid: number): void {
+		this.queue.set(mode, userid);
+	}
+
+	//get하고 없애는게 좋을 듯
+	removeFromQueueByMode(mode: number): void {
+		this.queue.delete(mode);
 	}
 }
