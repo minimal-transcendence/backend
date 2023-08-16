494023a6908238e2214ef6118c2994792f79261f
namkim <namkim@student.42seoul.kr>
2023-08-16 15:21:20 +0900

    namkim : amend sendChat argument format to nickname string

--- src/backend/src/chat/chat.service.ts
+++ src/backend/src/chat/chat.service.ts
@@ -243,7 +243,7 @@ export class ChatService {
 		if (room.userlist.has(client.data.id)){	//이렇게 아이디를 잘 가져올 수 있는지 생각해보자(auth 올라가면 그냥 client.id로 꺼내도 됨 (이건 사실 nickname도 그렇다))
 			if (!room.isMuted(client.data.id)){
 				const message = new Message(client.data.id, body);
-				io.in(to).emit("sendMessage", to, message);	//방에 emit(본인은 안 받아야)
+				io.in(to).emit("sendMessage", client.data.nickname, to, message);	//방에 emit(본인은 안 받아야)
 				room.messages.push(message);
 			}
 			else
