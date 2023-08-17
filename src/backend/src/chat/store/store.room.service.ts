92a6513b96eba4515b6420abf9a47e02f2c9a403
ysungwon <hwoarang09@naver.com>
2023-08-17 10:01:41 +0900

    y2

---
+++ src/backend/src/chat/store/store.room.service.ts
@@ -0,0 +1,175 @@
+import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import { Message } from './store.message.service';
+
+console.log('tt');
+//방도 number 로 관리해야한다 -> message from / to format
+export class Room {
+	//readonly 랑 const 차이?
+	//room을 private이랑 분리하게 되면 roomId 필요없는 듯
+	// readonly roomId : number;
+	// readonly roomname : string;
+	password : string | null;
+	owner : number;
+	operators : Set<number>;
+	userlist : Set<number>;	//있긴 있어야할듯... socket은 모든 접속에 id를 부여하니까ㅠ
+	//kick, ban, mute	//kicklist banlist 분리?
+	mutelist : Set<number>;
+	banlist : Set<number>;
+	messages : Message[];
+	isPrivate : boolean;
+
+	constructor(
+		// roomId: number,
+		owner : number,
+		password? : string,
+	){
+		// this.roomId = roomId;
+		// this.roomname = roomname;
+		this.password = password? password : null;
+		this.owner = owner;
+		this.operators = new Set();
+		this.userlist = new Set();
+		this.userlist.add(owner);
+		this.mutelist = new Set();
+		this.banlist = new Set();
+		this.messages = [];
+		this.isPrivate = false;
+	}
+
+	isPassword(input : string) : boolean {
+		return (input === this.password);
+	}
+
+	isOwner(userid : number) : boolean {
+		return (userid === this.owner);
+	}
+	
+	isOperator(userid : number) : boolean {
+		return (this.operators.has(userid));
+	}
+
+	isMuted(userid : number) : boolean {
+		return (this.mutelist.has(userid));
+	}
+
+	isJoinning(userid : number) : boolean {
+		return (this.userlist.has(userid));
+	}
+	
+	isBanned(userid : number) : boolean {
+		return (this.banlist.has(userid));
+	}
+
+	updatePassword(newPassword : string){
+		this.password = newPassword;
+	}
+
+	updateOwner(newOwner : number){
+		this.owner = newOwner;
+		// this.operators.delete(oldOwner);
+		// this.operators.add(newOwner);
+	}
+
+	//후... 이거 묶을 걸 그랬나ㅠ
+	addUserToUserlist(userid : number){
+		this.userlist.add(userid);
+	}
+
+	addUserToBanlist(userid : number){
+		//여기서 setTimeOut을 하면... 내가 원하는 대로 실행이 됨...?
+		//CHECK : 필수ㅠㅠ
+		this.banlist.add(userid);
+		setTimeout((userid : number) => {
+			this.banlist.delete(userid);
+		}, 60000);	//60초 뒤에...
+	}
+
+	deleteUserFromUserlist(userid : number){
+		this.userlist.delete(userid);
+	}
+
+	addUserToOperators(userid : number){
+		this.operators.add(userid);
+	}
+
+	deleteUserFromOperators(userid : number){
+		this.operators.delete(userid);
+	}
+
+	addUserToMutelist(userid : number){
+		this.mutelist.add(userid);
+		setTimeout((userid : number) => {
+			this.mutelist.delete(userid);
+		}, 60000);
+	}
+
+	//자동으로 일정시간만?
+	// deleteUserFromMutelist(userid : number){
+	// 	this.mutelist.delete(userid);
+	// }
+	private clearSets(){
+		this.userlist.clear();
+		this.operators.clear();
+		this.mutelist.clear();
+		this.banlist.clear();
+	}
+
+	clearRoom(){
+		// this.clearSets();
+		this.operators = null;
+		this.mutelist = null;
+		this.banlist = null;
+		this.userlist = null;
+	}
+
+	storeMessage(from : number, body : string){
+		this.messages.push(new Message(from, body));
+	}
+}
+
+interface RoomStore {
+  //방을 DM이랑 분리하게 되면 아이디는 필요없다!
+  rooms: Map<string, Room>;
+
+  findRoom(roomName: string): Room;
+  saveRoom(roomName: string, room: Room): void;
+  findAllRoom(): Room[];
+}
+
+@Injectable()
+export class ChatRoomStoreService implements RoomStore{
+	rooms = new Map();
+
+	findRoom(roomName: string): Room {
+		return this.rooms.get(roomName);
+	}
+
+	//여기는 password가 필요하다
+	saveRoom(roomname: string, room: Room): void {
+		this.rooms.set(roomname, room);
+	}
+
+	findAllRoom(): Room[] {
+		return [...this.rooms.values()];
+	}
+
+	deleteRoom(roomname: string) : void {
+		const target = this.rooms.get(roomname);
+		if (target !== undefined)
+			target.clearRoom();
+		this.rooms.delete(roomname);
+	}
+
+	//
+	findQueryMatchRoomNames(query : string) : string[] {
+		const res = [];
+		this.rooms.forEach((_, key) => {
+			if (key.includes(query)){
+				if (this.findRoom(key).isPrivate == false)
+					res.push(key);
+			}
+		})
+		return (res);
+	}
+}
---
+++ src/frontend/srcs/UserList.tsx
@@ -0,0 +1,267 @@
+import React, { useState, useEffect } from 'react';
+import styles from "./UserListStyle.module.css";
+
+function UserList() {
+	const [showModals, setShowModals] = useState<boolean[]>([]);
+	const [showprofileOption, setShowprofileOption] = useState(true);
+	const [userNickname, setUserNickname] = useState<string | null>(localStorage.getItem("nickname"));
+	const [userId, setUserID] = useState<string | null>(localStorage.getItem("id"));
+	const [userData, setData] = useState<userDataInterface[]>([]);
+
+	interface userMatchHistory{
+		winner: string,
+		loser: string,
+		time: string,
+	}
+
+	interface userDataInterface{
+		id: string,
+		nickname: string,
+		userProfileURL: string,
+		win: number,
+		lose: number,
+		score: number,
+		lastLogin: string,
+		isFriend: number,
+		matchhistory: userMatchHistory[],
+	}
+
+	function checkIsFriend(id:string[], userid:string){
+		if (id.includes(userid)){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+
+	const reloadData = async() => {
+		setData([]);
+		setShowModals([]);
+
+		const idList:string[] = [];
+		const responseFriend = await (await fetch ('http://localhost/api/user/' + userId + '/friend')).json();
+		const friendCount = responseFriend.friendList.length;
+		for(let i = 0; i < friendCount ; i++){
+			idList.push(responseFriend.friendList[i].id);
+		}
+
+		const response = await(await fetch('http://localhost/api/user')).json();
+		const useridx = response.length;
+
+		const newDataList: userDataInterface[] = [];
+		const newModalList: boolean[] = [];
+		for(let i = 0 ; i < useridx ; i++){
+			const detailResponse = await(await fetch('http://localhost/api/user/' + response[i].id)).json();
+			const matchResponse = await(await fetch('http://localhost/api/user/' + response[i].id + '/matchhistory')).json();
+			const matchCount = matchResponse.length;
+			const newData: userDataInterface = {
+				id: detailResponse.id,
+				nickname: detailResponse.nickname,
+				userProfileURL: "/api/" + detailResponse.avatar,
+				score: parseInt(detailResponse.score),
+				win: detailResponse._count.asWinner,
+				lose: detailResponse._count.asLoser,
+				lastLogin: detailResponse.lastLogin,
+				isFriend: checkIsFriend(idList, detailResponse.id),
+				matchhistory: [],
+			};
+			for(let i = 0 ; i < matchCount ; i++){
+				const newMatchData: userMatchHistory = {
+					winner: matchResponse[i].winner.nickname,
+					loser: matchResponse[i].loser.nickname,
+					time: matchResponse[i].createdTime,
+				};
+				newMatchData.time = newMatchData.time.slice(0,10);
+				newData.matchhistory.push(newMatchData);
+			}
+			newDataList.push(newData);
+			newModalList.push(false);
+		}
+		setData(newDataList);
+		setShowModals(newModalList);
+	}
+
+	function profilePopup(index:number){
+		let copiedData = [...showModals];
+		copiedData[index] = true;
+		setShowModals(copiedData);
+	}
+
+	function profilePopdown(index:number){
+		let copiedData = [...showModals];
+		copiedData[index] = false;
+		setShowModals(copiedData);
+	}
+
+	function sendGameMatch(index:number){
+		//매치신청 보내기
+	}
+
+	async function follow(index:number){
+		const apiUrl = 'http://localhost/api/user/' + userId + '/friend';
+		const dataToUpdate = {
+			// 업데이트하고자 하는 데이터 객체
+			id: userId,
+			isAdd: true,
+			friend: userData[index].id,
+		};
+
+		try {
+			const response = await fetch(apiUrl, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(dataToUpdate),
+			});
+
+			if (!response.ok) {
+			throw new Error('API 요청이 실패하였습니다.');
+			}
+
+			const responseData = await response.json();
+
+			console.log('Follow 응답 데이터:', responseData);
+			let copiedData = [...userData];
+			copiedData[index].isFriend = 1;
+			setData(copiedData);
+			reloadData();
+		} catch (error) {
+			alert("Follow에 실패했습니다");
+			console.error('에러 발생:', error);
+		}
+	}
+
+	async function unFollow(index:number){
+		const apiUrl = 'http://localhost/api/user/' + userId + '/friend';
+		const dataToUpdate = {
+			// 업데이트하고자 하는 데이터 객체
+			id: userId,
+			isAdd: false,
+			friend: userData[index].id,
+		};
+
+		try {
+			const response = await fetch(apiUrl, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(dataToUpdate),
+			});
+
+			if (!response.ok) {
+			throw new Error('API 요청이 실패하였습니다.');
+			}
+
+			const responseData = await response.json();
+
+			console.log('unFollow 응답 데이터:', responseData);
+			let copiedData = [...userData];
+			copiedData[index].isFriend = 1;
+			setData(copiedData);
+			reloadData();
+		} catch (error) {
+			alert("Follow에 실패했습니다");
+			console.error('에러 발생:', error);
+		}
+	}
+
+	useEffect (() => {
+		reloadData();
+		}, []);
+
+		function getProfile(index:number){
+			if (showprofileOption || userData[index].isFriend){
+				return(
+				<>
+				<div className={styles.profileBox}>
+					<div className={styles.profileImage}>
+						{userData[index].userProfileURL !== '/apipath' ? (
+						<img src={userData[index].userProfileURL} alt="profile image" className={styles.profileImage} />) :
+						(<img src="img/img1.png" alt="profile image" className={styles.profilePicture} />)}
+					</div>
+					<div className={styles.profileInfo}>
+						<h2>{userData[index].nickname}</h2>
+						<h3>승: {userData[index].win} 패:{userData[index].lose}</h3>
+						<div className={styles.buttons}>
+							{userData[index].isFriend === 1 && (
+								<button className={styles.unfollowIn} onClick={() => {unFollow(index)}}>
+									언팔로우
+								</button>)}
+							{userData[index].isFriend === 0 && (
+								<button className={styles.followIn} onClick={() => {follow(index)}}>
+									팔로우
+								</button>)}
+							<button className={styles.normalIn} onClick={() => {sendGameMatch(index)}}>
+									게임 신청
+							</button>
+							<button className={styles.normalIn} onClick={() => {profilePopup(index)}}>
+									프로필 보기
+							</button>
+						</div>
+					</div>
+				</div>
+			{showModals[index] && (
+			<div className='modal'>
+				<div className='modal-content'>
+					<p>
+						{userData[index].userProfileURL != '/apipath' ? (
+						<img src={userData[index].userProfileURL} alt="profile image" width="100" height = "100" />) :
+						(<img src="img/img1.png" alt="profile image" width="100" height = "100" />)}
+					</p>
+					<h2>
+						{userData[index].nickname} ({userData[index].id}) 의 프로필
+					</h2>
+					<p>승: {userData[index].win} 패:{userData[index].lose} 점수: {userData[index].win * 10 - userData[index].lose * 10}</p>
+					<p>최근 전적</p>
+					<p>
+					{userData[index].matchhistory.map((item, idx) => (
+					<div key={idx}>
+						{userData[index].matchhistory[idx].time} 승: {userData[index].matchhistory[idx].winner} 패: {userData[index].matchhistory[idx].loser}
+					</div>
+					))}
+					</p>
+						{userData[index].nickname !== userNickname && userData[index].isFriend === 1 && (
+						<button onClick={() => {unFollow(index)}}>언팔로우</button>)}
+						{userData[index].nickname !== userNickname && userData[index].isFriend === 0 && (
+						<button onClick={() => {follow(index)}}>팔로우</button>)}
+						{userData[index].nickname !== userNickname && (
+						<button>게임 신청</button>)}
+					<p>
+					<button onClick={() => profilePopdown(index)}>닫기</button>
+					</p>
+				</div>
+			</div>
+			)}
+			</>
+		);
+		}
+		else {
+			return null;
+		}
+		}
+
+		return (
+			<div>
+				{showprofileOption === false && (
+					<button onClick={() => setShowprofileOption(true)}>전체 보기</button>
+				)}
+				{showprofileOption === true && (
+					<button onClick={() => setShowprofileOption(false)}>친구만 보기</button>
+				)}
+				<button onClick={() => reloadData()}>새로 고침</button>
+				<div className={styles.profileMainBox}>
+					{userData.map((item, index) => (
+					<div key={index}>
+						{userId && userData[index].id != userId && (
+						getProfile(index)
+						)}
+					</div>
+					))}
+				</div>
+			</div>
+			);
+		}
+
+export default UserList;
