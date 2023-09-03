6977b0ff21278c301ea65f0a0448dba6a29bb517
namkim <0skyblue@naver.com>
2023-09-03 09:24:34 +0900

    namkim : update : updateUserAvatar in ChatGateway & userService

--- src/backend/src/socket.io/chat/chat.gateway.ts
+++ src/backend/src/socket.io/chat/chat.gateway.ts
@@ -1,12 +1,12 @@
 import { Logger, ValidationPipe, UsePipes, UseFilters } from '@nestjs/common';
 import {
-  ConnectedSocket,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
-  OnGatewayInit,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
+	ConnectedSocket,
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+	OnGatewayInit,
+	SubscribeMessage,
+	WebSocketGateway,
+	WebSocketServer,
 } from '@nestjs/websockets';
 import { Namespace } from 'socket.io';
 import { ChatService } from './chat.service';
@@ -21,21 +21,20 @@ import { WsExceptionFilter } from '../ws-exception.filter';
 	namespace: 'chat',
 })
 export class ChatGateway
-  implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
-{
-	@WebSocketServer() io : Namespace;
+	implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
+	@WebSocketServer() io: Namespace;
 	private readonly logger = new Logger(ChatGateway.name);
-	constructor(private chatService: ChatService) {}
+	constructor(private chatService: ChatService) { }
 
-	afterInit(){
+	afterInit() {
 		this.chatService.initChatServer();
 	}
 
 	handleConnection(@ConnectedSocket() client: ChatSocket) {
 		//db 랑 연동할 것인지...?
-	    this.logger.log(`Client Connected : ${client.nickname}, ${client.id}`);
-	
-		client.onAny((any : any) => {
+		this.logger.log(`Client Connected : ${client.nickname}, ${client.id}`);
+
+		client.onAny((any: any) => {
 			this.logger.log(`From Client ${client.nickname}, ${client.id}, event : ${any}`);
 		})
 		this.chatService.handleNewConnection(this.io, client);
@@ -49,165 +48,165 @@ export class ChatGateway
 
 
 	@SubscribeMessage('sendChatMessage')
-	handleSendChat(client: ChatSocket, payload : MessageDto){
+	handleSendChat(client: ChatSocket, payload: MessageDto) {
 		this.chatService.sendChat(
 			this.io, client, payload.to, payload.body
 		);
 	}
 
 	@SubscribeMessage('sendDirectMessage')
-	handleSendDM(client: ChatSocket, payload : MessageDto){
+	handleSendDM(client: ChatSocket, payload: MessageDto) {
 		this.chatService.fetchDM(
 			this.io, client, payload.to, payload.body
 		);
 	}
 
 	@SubscribeMessage('selectRoom')
-	handleEnterRoom(client: ChatSocket, payload : RoomDto){
+	handleEnterRoom(client: ChatSocket, payload: RoomDto) {
 		this.chatService.userJoinRoom(
 			this.io, client, payload.roomname
 		);
 	}
 
 	@SubscribeMessage('selectDMRoom')
-	handleEnterDMRoom(client: ChatSocket, payload : TargetDto){
+	handleEnterDMRoom(client: ChatSocket, payload: TargetDto) {
 		this.chatService.fetchUserToDMRoom(
 			client, payload.target
 		);
 	}
 
 	@SubscribeMessage('sendRoomLeave')
-	handleLeaveRoom(client: ChatSocket, payload : RoomDto){
+	handleLeaveRoom(client: ChatSocket, payload: RoomDto) {
 		this.chatService.userLeaveRoom(this.io, client, payload.roomname);
-    	this.chatService.userLeaveRoomAct(this.io, client.userId, payload.roomname);
+		this.chatService.userLeaveRoomAct(this.io, client.userId, payload.roomname);
 	}
 
 	@SubscribeMessage('sendRoomPass')
-	handleEnterRoomWithPassword(client: ChatSocket, payload : RoomDto){
+	handleEnterRoomWithPassword(client: ChatSocket, payload: RoomDto) {
 		this.chatService.userJoinRoom(
 			this.io, client, payload.roomname, payload.password
 		);
 	}
 
 	@SubscribeMessage('setRoomPass')
-	handleSetRoomPass(client: ChatSocket, payload : RoomDto){
+	handleSetRoomPass(client: ChatSocket, payload: RoomDto) {
 		this.chatService.setPassword(
 			this.io, client, payload.roomname, payload.password
 		);
 	}
 
 	@SubscribeMessage('setRoomPrivate')
-	handleSetRoomPrivate(client: ChatSocket, payload : RoomDto){
+	handleSetRoomPrivate(client: ChatSocket, payload: RoomDto) {
 		this.chatService.setRoomStatus(
 			this.io, client, payload.roomname, true
 		);
 	}
 	@SubscribeMessage('setRoomPublic')
-	handleSetRoomPulbic(client: ChatSocket, payload : RoomDto){
+	handleSetRoomPulbic(client: ChatSocket, payload: RoomDto) {
 		this.chatService.setRoomStatus(
 			this.io, client, payload.roomname, false
 		);
 	}
 
 	@SubscribeMessage('blockUser')
-	handleBlockUser(client: ChatSocket, payload : TargetDto){
+	handleBlockUser(client: ChatSocket, payload: TargetDto) {
 		this.chatService.blockUser(
 			this.io, client, payload.target
 		);
 	}
 
 	@SubscribeMessage('unblockUser')
-	handleUnblockUser(client: ChatSocket, payload : TargetDto){
+	handleUnblockUser(client: ChatSocket, payload: TargetDto) {
 		this.chatService.unblockUser(
 			this.io, client, payload.target
 		);
 	}
-	
+
 	//과연...?
 	@SubscribeMessage('kickUser')
-	handleKickUser(client: ChatSocket, payload : RoomEventDto){
+	handleKickUser(client: ChatSocket, payload: RoomEventDto) {
 		this.chatService.kickUser(
 			this.io, client, payload.roomname, payload.target
 		);
 	}
-	
+
 	@SubscribeMessage('banUser')
-	handleBanUser(client: ChatSocket, payload : RoomEventDto){
+	handleBanUser(client: ChatSocket, payload: RoomEventDto) {
 		this.chatService.banUser(
 			this.io, client, payload.roomname, payload.target
 		);
 	}
-	
+
 	@SubscribeMessage('muteUser')
-	handleMuteUser(client: ChatSocket, payload : RoomEventDto){
+	handleMuteUser(client: ChatSocket, payload: RoomEventDto) {
 		this.chatService.muteUser(
 			this.io, client, payload.roomname, payload.target
 		);
 	}
-	
+
 	@SubscribeMessage('addOperator')
-	handleAddOper(client: ChatSocket, payload : RoomEventDto){
+	handleAddOper(client: ChatSocket, payload: RoomEventDto) {
 		this.chatService.addOperator(
 			this.io, client, payload.roomname, payload.target
 		);
 	}
-	
+
 	@SubscribeMessage('deleteOperator')
-	handleDelOper(client: ChatSocket, payload : RoomEventDto){
+	handleDelOper(client: ChatSocket, payload: RoomEventDto) {
 		this.chatService.deleteOperator(
 			this.io, client, payload.roomname, payload.target
 		);
 	}
 
 	@SubscribeMessage('requestAllRoomList')
-	handleReqAllRoomList(client: ChatSocket){
+	handleReqAllRoomList(client: ChatSocket) {
 		const roomInfo = this.chatService.getAllRoomList(client.userId);
 		client.emit('sendRoomList', roomInfo);
 	}
-	
+
 	@SubscribeMessage('requestMyRoomList')
-	handleReqUserRoomList(client: ChatSocket){
-    	const roomInfo = this.chatService.getUserRoomList(client.userId);
+	handleReqUserRoomList(client: ChatSocket) {
+		const roomInfo = this.chatService.getUserRoomList(client.userId);
 		client.emit('sendRoomList', roomInfo);
 
 	}
-	
+
 	@SubscribeMessage('requestAllMembers')
-	handleReqAllMembers(client: ChatSocket){
+	handleReqAllMembers(client: ChatSocket) {
 		const members = this.chatService.getAllUserInfo(client.userId);
-    	client.emit('responseAllMembers', members);
+		client.emit('responseAllMembers', members);
 	}
 
-	
+
 	@SubscribeMessage('requestRoomMembers')
-	handleReqRoomMembers(client: ChatSocket, payload : RoomDto){
+	handleReqRoomMembers(client: ChatSocket, payload: RoomDto) {
 		const roomMembers = this.chatService.makeRoomUserInfo(payload.roomname);
 		client.emit('sendRoomMembers', roomMembers);
 	}
 
 	@SubscribeMessage('requestTargetMember')
-	handleReqTargetMember(client: ChatSocket, payload : TargetDto){
+	handleReqTargetMember(client: ChatSocket, payload: TargetDto) {
 		const member = this.chatService.getUserInfoById(client.userId, payload.target);
 		client.emit('responseTargetMember', member);
 	}
-	
+
 	@SubscribeMessage('requestSearchResultRoomList')
-	handleReqQueryRes(client: ChatSocket, payload : TargetDto){
+	handleReqQueryRes(client: ChatSocket, payload: TargetDto) {
 		const roomInfo = this.chatService.getQueryRoomList(client.userId, payload.target);
-      client.emit('responseRoomQuery', roomInfo);
+		client.emit('responseRoomQuery', roomInfo);
 	}
 
-	updateUserNick(userId : number, newNick : string) {
+	updateUserNick(userId: number, newNick: string) {
 		this.io.emit("updateUserNick", userId, newNick);
-    this.chatService.userChangeNick(this.io, userId, newNick);
+		this.chatService.userChangeNick(this.io, userId, newNick);
 	}
 
-	updateUserAvatar(userId : number){
-		this.io.emit("updateUserAvatar", userId);
+	updateUserAvatar(userId: number, filePath: string) {
+		this.io.emit("updateUserAvatar", userId, filePath);
 	}
 
 	//check handleDisconnect
-	logout(clientId : number) {
+	logout(clientId: number) {
 		this.io.in(`$${clientId}`).disconnectSockets(true); //false?
 	}
 }
