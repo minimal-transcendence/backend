ef1857e874cefcdbf993298c5981e1fe0e1fc878
ysungwon <hwoarang09@naver.com>
2023-09-07 06:15:57 +0900

    randomMatch animation

--- src/frontend/pages/components/navpage/NavBar.tsx
+++ src/frontend/pages/components/navpage/NavBar.tsx
@@ -14,31 +14,7 @@ export default function NavBar({
   setLeftHeader: any;
   setError: any;
 }) {
-  // const socket = useContext(SocketContext).chatSocket;
   console.log("navebar ");
-
-  //   <div>
-  //   <button onClick={() => setMyProfileModal(true)}>내 프로필</button>
-  //   <button onClick={() => setUserListModal(true)}>유저 목록</button>
-  //   <button onClick={logout}>로그 아웃</button>
-  // </div>
-  // <div>
-  //   {userListModal && (
-  //     <>
-  //       <button >닫기</button>
-  //       <UserList />
-  //     </>
-  //   )}
-  // </div>
-  // <div>
-  //   {myProfileModal && (
-  //     <>
-  //       <button >닫기</button>
-  //       <MyProfile />
-  //     </>
-  //   )}
-  // </div>
-  //     {/*<UserProfile id='1' /> 유저 프로필만 가져올때는 이렇게 사용 id는 보고싶은 유저의 id*/}
 
   return (
     <nav className="nav-bar">
