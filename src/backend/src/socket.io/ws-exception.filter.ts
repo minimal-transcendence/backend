cf161c65f67cd092c1fb598bb920b838e1345f1d
SeungJoonChoi <tmdwns0845@naver.com>
2023-09-06 19:06:04 +0900

    fix(ws filter)

--- src/backend/src/socket.io/ws-exception.filter.ts
+++ src/backend/src/socket.io/ws-exception.filter.ts
@@ -1,23 +1,10 @@
-import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
 import { WsException } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
 
 @Catch(WsException, HttpException)
-export class WsExceptionFilter {
-  public catch(exception: HttpException, host: ArgumentsHost) {
-    const client = host.switchToWs().getClient();
-    this.handleError(client, exception);
-  }
-
-  public handleError(client: Socket, exception: HttpException | WsException) {
-    if (exception instanceof HttpException) {
-      // handle http exception
-      console.log("HttpException: invalid data transfer");
-      console.log(exception.message)
-    } else {
-      // handle websocket exception
-      console.log("WsException: invalid data transfer");
-      console.log(exception.message)
-    }
+export class WsExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const payload = host.switchToWs().getData();
+    console.log(`${exception.name} - invalid data transfer: ${payload}`);
   }
 }
