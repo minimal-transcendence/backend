df488c875816a180f791559fe05ada65084dcf08
SeungJoonChoi <tmdwns0845@naver.com>
2023-09-08 14:40:43 +0900

    fix(backend): convert console.log to Logger

--- src/backend/src/socket.io/ws-exception.filter.ts
+++ src/backend/src/socket.io/ws-exception.filter.ts
@@ -1,10 +1,13 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, Logger } from '@nestjs/common';
 import { WsException } from '@nestjs/websockets';
 
 @Catch(WsException, HttpException)
 export class WsExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(WsExceptionFilter.name);
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const payload = host.switchToWs().getData();
-    console.log(`${exception.name} - invalid data transfer: ${payload}`);
+    this.logger.error(`${exception.name} - invalid data transfer: ${payload}`);
+    // console.log(`${exception.name} - invalid data transfer: ${payload}`);
   }
 }
